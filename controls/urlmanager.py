8386a066162581911fde9d04a20670bfadf5ff30
fidelsurtida <fidelsurtida@gmail.com>
2024-08-06 10:04:14 +0800

    Created a new Custom Control URLManager to separate codes.

---
+++ controls/urlmanager.py
@@ -0,0 +1,163 @@
+# ---------------------------------------------------
+# urlmanager.py - URLManager Class
+# ---------------------------------------------------
+# A custom flet control that manages the added
+# urls and validates them. It shows dialog boxes
+# which contain error messages to the related URL.
+# This also fetches the gsheet data on a valid
+# URL input and converts them to gsheeturl controls.
+# ---------------------------------------------------
+
+import flet as ft
+import json
+from pathlib import Path
+from controls.gsheeturl import GSheetURL
+from controls.gsheetlister import GSheetLister
+from controls.progress import Progress
+from modules.reader import Reader
+from modules.styles import Styles
+
+
+class URLManager(ft.Card):
+
+    def __init__(self, *, gsheetlister_control: GSheetLister,
+                 progress_control: Progress, download_btn):
+        """
+        Custom Flet Control for Managing the added URLs.
+        It will show respective error dialog boxes on
+        invalid URLS and will convert valid into
+        gsheeturl controls and add it to gsheetlister.
+        """
+        super().__init__()
+
+        # Passed Reference of the GSheetLister Custom Control
+        # Download CSV Button and Progress Controls
+        self.gsheetlister = gsheetlister_control
+        self.download_button = download_btn
+        self.progressbar = progress_control
+
+        # Declaration of Flet Control References
+        self._gsheet_url = ft.Ref[ft.TextField]()
+        self._add_url_button = ft.Ref[ft.ElevatedButton]()
+
+        # Initialize the Custom UI Design
+        hint = "https://docs.google.com/spreadsheets/..."
+        self.content = ft.Container(
+            content=ft.Row([
+                ft.TextField(ref=self._gsheet_url,
+                             label="Google Sheet URL:",
+                             border_color=ft.colors.GREY_500,
+                             hint_text=hint,
+                             expand=4),
+                ft.ElevatedButton(ref=self._add_url_button,
+                                  text="ADD URL",
+                                  icon="add_link_sharp", height=50,
+                                  expand=1,
+                                  style=Styles.add_url_style,
+                                  on_click=self._add_url_button_event),
+                ft.OutlinedButton(content=ft.Icon("settings_rounded",
+                                  size=30, color=ft.colors.WHITE60),
+                                  width=55, height=50,
+                                  style=Styles.settings_style,
+                                  tooltip="DATA SETTINGS ")
+            ], spacing=15), padding=20)
+
+    def _add_url_button_event(self, e):
+        """
+        This button event will add a GSheetURL control to
+        the container UI interface and perform a data
+        fetch on the Google Sheet.
+        """
+        url = self._gsheet_url.current.value
+
+        # Perform a validation first on the url if it exists already on data
+        if url in self.gsheetlister.URLS_DB.keys():
+            data = self.gsheetlister.URLS_DB[url]
+            dialogbox = self._generate_existing_url_dialogbox(data)
+            self._gsheet_url.current.value = ""
+            e.page.open(dialogbox)
+            e.page.update()
+            return
+
+        # Trigger first the recently added event to load recents list
+        self.gsheetlister.show_recently_added()
+
+        # Then proceed adding and creating the gsheet URL
+        if url:
+            # Create a GSheetURL control and append it to the gsheets cont
+            gsheeturl_control = GSheetURL(url)
+            self._gsheet_url.current.value = ""
+            self.gsheetlister.append(gsheeturl_control, first=True)
+            self.gsheetlister.disable_filter_controls(True)
+            self._add_url_button.current.disabled = True
+            self.download_button.current.disabled = True
+            self.progressbar.reset()
+            e.page.update()
+
+            def fetch_completed(**kwargs):
+                """ Callback method after the data fetch has been completed. """
+                # DATA[kwargs["owner"]] = kwargs["final_data"]
+                gsheeturl_control.update_display_labels(
+                    owner=kwargs["owner"], month=kwargs["month"],
+                    timestamp=kwargs["timestamp"], diskload=False)
+                # Update the states of UI Controls
+                self._add_url_button.current.disabled = False
+                self.download_button.current.disabled = False
+                self.gsheetlister.disable_filter_controls(False)
+                e.page.update()
+
+                # Save the downloaded data to its own JSON file
+                data_dir = Path(Reader.BASE_PATH / "downloads/data")
+                data_dir.mkdir(parents=True, exist_ok=True)
+                owner_formatted = kwargs["owner"].lower().replace(" ", "-")
+                filename = f"{kwargs["month_num"]}-{owner_formatted}.json"
+                file = data_dir / filename
+                with open(file, "w") as outfile:
+                    json.dump(kwargs, outfile)
+
+                # Save to the gsheetlister RECENTS list
+                self.gsheetlister.add_recents(filename)
+                # Save to the gsheetlister URLS_DB dictionary
+                month, year = kwargs["month"].split()
+                month_num = kwargs["month_num"].split("-")[0]
+                self.gsheetlister.add_urlsdb(url=kwargs["url"], month=month,
+                                             year=year, month_num=month_num,
+                                             owner=kwargs["owner"])
+
+            def progress_callback(**kwargs):
+                """ Callback for the progress bar control to update. """
+                self.progressbar.update_progress(**kwargs)
+
+            # Create Reader class to fetch data and pass the required callbacks
+            reader = Reader(url=url)
+            reader.fetch_data(sheet_identifier="*-",
+                              progress=progress_callback,
+                              completed=fetch_completed)
+
+    def _generate_existing_url_dialogbox(self, data):
+        """ Helper method to generate a dialogbox control for existing urls. """
+        def proceed_event(ev):
+            # Callback event for the proceed button
+            ev.page.close(alert_dialog)
+            self.gsheetlister.filter_gsheeturl(month=data["month_num"],
+                                               year=data["year"])
+
+        alert_dialog = ft.AlertDialog(
+            title=ft.Row([
+                ft.Icon("warning_rounded", color=ft.colors.ORANGE_ACCENT,
+                        size=32),
+                ft.Text("GSHEET URL EXISTS", weight=ft.FontWeight.BOLD,
+                        text_align=ft.TextAlign.CENTER, size=20)], spacing=10),
+            content=ft.Text(f"This URL is for {data['owner'].upper()}"
+                            f" that is added on {data['month']} "
+                            f"{data['year']}.\nWould you like to view the "
+                            f"saved GSheet URL?"),
+            actions=[
+                ft.TextButton("Cancel",
+                              on_click=lambda a: a.page.close(alert_dialog)),
+                ft.ElevatedButton("Proceed", on_click=proceed_event,
+                                  bgcolor=ft.colors.BLUE_ACCENT,
+                                  color=ft.colors.WHITE)],
+            modal=True, bgcolor=ft.colors.GREY_900)
+
+        return alert_dialog
--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import flet as ft
-import json
-from pathlib import Path
-from controls.gsheeturl import GSheetURL
+from controls.urlmanager import URLManager
 from controls.gsheetlister import GSheetLister
 from controls.progress import Progress
 from modules.reader import Reader
@@ -12,111 +10,14 @@ from modules.styles import Styles
 DATA = {}
 
 # Flet Control References
-gsheet_url = ft.Ref[ft.TextField]()
 download_button = ft.Ref[ft.ElevatedButton]()
-add_url_button = ft.Ref[ft.ElevatedButton]()
 
 # Custom Control References
 gsheetlister_control = GSheetLister()
 progressbar_control = Progress()
-
-
-def add_url_button_event(e):
-    """
-    This button event will add a GSheetURL control to
-    the container UI interface and perform a data
-    fetch on the Google Sheet.
-    """
-    url = gsheet_url.current.value
-
-    # Perform a validation first on the url if it exists already on data
-    if url in gsheetlister_control.URLS_DB.keys():
-        data = gsheetlister_control.URLS_DB[url]
-
-        def proceed_event(ev):
-            ev.page.close(dlg)
-            gsheetlister_control.filter_gsheeturl(month=data["month_num"],
-                                                  year=data["year"])
-
-        dlg = ft.AlertDialog(
-            title=ft.Row([
-                ft.Icon("warning_rounded", color=ft.colors.ORANGE_ACCENT,
-                        size=32),
-                ft.Text("GSHEET URL EXISTS", weight=ft.FontWeight.BOLD,
-                        text_align=ft.TextAlign.CENTER, size=20)
-            ], spacing=10),
-            content=ft.Text(f"This URL is for {data['owner'].upper()}"
-                            f" that is added on {data['month']} "
-                            f"{data['year']}.\nWould you like to view the "
-                            f"saved GSheet URL?"),
-            actions=[
-                ft.TextButton("Cancel", on_click=lambda a: a.page.close(dlg)),
-                ft.ElevatedButton("Proceed", on_click=proceed_event,
-                                  bgcolor=ft.colors.BLUE_ACCENT,
-                                  color=ft.colors.WHITE)
-            ],
-            modal=True, bgcolor=ft.colors.GREY_900)
-
-        gsheet_url.current.value = ""
-        e.page.open(dlg)
-        e.page.update()
-        return
-
-    # Trigger first the recently added event to load recents list
-    gsheetlister_control.show_recently_added()
-
-    # Then proceed adding and creating the gsheet URL
-    if url:
-        # Create a GSheetURL control and append it to the gsheets cont
-        gsheeturl_control = GSheetURL(url)
-        gsheet_url.current.value = ""
-        gsheetlister_control.append(gsheeturl_control, first=True)
-        gsheetlister_control.disable_filter_controls(True)
-        add_url_button.current.disabled = True
-        download_button.current.disabled = True
-        progressbar_control.reset()
-        e.page.update()
-
-        def fetch_completed(**kwargs):
-            """ Callback method after the data fetch has been completed. """
-            DATA[kwargs["owner"]] = kwargs["final_data"]
-            gsheeturl_control.update_display_labels(
-                owner=kwargs["owner"], month=kwargs["month"],
-                timestamp=kwargs["timestamp"], diskload=False)
-            # Update the states of UI Controls
-            add_url_button.current.disabled = False
-            download_button.current.disabled = False
-            gsheetlister_control.disable_filter_controls(False)
-            e.page.update()
-
-            # Save the downloaded data to its own JSON file
-            data_dir = Path(Reader.BASE_PATH / "downloads/data")
-            data_dir.mkdir(parents=True, exist_ok=True)
-            owner_formatted = kwargs["owner"].lower().replace(" ", "-")
-            filename = f"{kwargs["month_num"]}-{owner_formatted}.json"
-            file = data_dir / filename
-            with open(file, "w") as outfile:
-                json.dump(kwargs, outfile)
-
-            # Save to the gsheetlister RECENTS list
-            gsheetlister_control.add_recents(filename)
-            # Save to the gsheetlister URLS_DB dictionary
-            month, year = kwargs["month"].split()
-            month_num = kwargs["month_num"].split("-")[0]
-            gsheetlister_control.add_urlsdb(url=kwargs["url"],
-                                            month=month, year=year,
-                                            month_num=month_num,
-                                            owner=kwargs["owner"])
-
-        def progress_callback(**kwargs):
-            """ Callback for the progress bar control to update. """
-            progressbar_control.update_progress(**kwargs)
-
-        # Create a Reader class to fetch data and pass the required callbacks
-        reader = Reader(url=url)
-        reader.fetch_data(sheet_identifier="*-",
-                          progress=progress_callback,
-                          completed=fetch_completed)
+urlmanager_control = URLManager(gsheetlister_control=gsheetlister_control,
+                                progress_control=progressbar_control,
+                                download_btn=download_button)
 
 
 def download_button_event():
@@ -144,28 +45,6 @@ def main(page: ft.Page):
     page.window.resizable = False
     page.theme_mode = ft.ThemeMode.DARK
 
-    # Google Sheet Adder Card
-    hint = "https://docs.google.com/spreadsheets/..."
-    sheet_url_card = ft.Card(content=ft.Container(
-        content=ft.Row([
-            ft.TextField(ref=gsheet_url,
-                         label="Google Sheet URL:",
-                         border_color=ft.colors.GREY_500,
-                         hint_text=hint,
-                         expand=4),
-            ft.ElevatedButton(ref=add_url_button,
-                              text="ADD URL",
-                              icon="add_link_sharp", height=50,
-                              expand=1,
-                              style=Styles.add_url_style,
-                              on_click=add_url_button_event),
-            ft.OutlinedButton(content=ft.Icon("settings_rounded",
-                              size=30, color=ft.colors.WHITE60),
-                              width=55, height=50,
-                              style=Styles.settings_style,
-                              tooltip="DATA SETTINGS ")
-        ], spacing=15), padding=20))
-
     # Download Button and Progress Bar Container
     download_progress_container = ft.Container(
         content=ft.Row([
@@ -181,7 +60,7 @@ def main(page: ft.Page):
         ), height=70, padding=10)
 
     # Add the main elements of the application
-    page.add(sheet_url_card)
+    page.add(urlmanager_control)
     page.add(gsheetlister_control)
     page.add(download_progress_container)
 
